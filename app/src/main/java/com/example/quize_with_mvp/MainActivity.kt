188873897f14b310dbc38a5ce6674abc5cf12711
godgo <godgod153@gmail.com>
2021-10-01 01:44:54 +0900

    MVP프로젝트  생성

---
+++ app/src/main/java/com/example/quize_with_mvp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.quize_with_mvp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
